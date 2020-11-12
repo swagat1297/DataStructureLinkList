1025baa434f44f04f9d56bf72bcd8687cb31d3e7
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-12 20:38:50 +0530

    Added: newLinkList delete firstnode

---
+++ main/newLinkList.java
@@ -0,0 +1,66 @@
+
+public class newLinkList<E> {
+	public INodei tail;
+	public INodei head;
+	
+	public newLinkList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	public void add(INodei valueNode) {
+		if(this.head == null) {
+			this.head = valueNode;
+		}
+		if(this.tail == null) {
+			this.tail = valueNode;
+		}
+		else {
+			INodei nextNodei = this.head;;
+			this.head = valueNode;
+			this.head.setNext(nextNodei);
+		}
+	}
+	public INodei append(INodei valueNode){
+		if(this.head == null) {
+			this.head = valueNode;
+		}
+		if(this.tail == null) {
+			this.tail = valueNode;
+		}
+		else {
+			INodei tempNode = this.head;
+			this.head = head.getNext();
+			return tempNode;
+		}
+		return valueNode;
+	}
+	public INodei pop() {
+		
+		INodei tempNode = this.head;
+		this.head = head.getNext();
+		return tempNode;
+	}
+	public void insert(INodei myNode, INodei newNode) {
+		INodei tempNode = myNode.getNext();
+		myNode.setNext(newNode);
+		newNode.setNext(tempNode);
+	}
+	public MyNode RemoveFromStart() {
+	        MyNode tempNode = (MyNode) this.head;
+	        this.head = head.getNext();
+	        return tempNode;
+	    }
+	public void printNodeAfterAppending() {
+		StringBuffer newNodes = new StringBuffer("Nodes ");
+		INodei tempNode = head;
+		while(tempNode.getNext() != null) {
+			newNodes.append(tempNode.getKey());
+			if(!tempNode.equals(tail)) newNodes.append("->");
+			tempNode = tempNode.getNext();
+		}
+		newNodes.append(tempNode.getKey());
+		System.out.println(newNodes);
+	}
+		
+}
